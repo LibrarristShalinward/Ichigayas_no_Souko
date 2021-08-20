d3c3659eb76137cd488ee027587c7cb4357db5ef
LibrarristShalinward <1701427793@qq.com>
2021-08-20 13:43:06 +0800

    完成了Ichigaya.chart.chart相关私有功能

--- Ichigaya/chart.py
+++ Ichigaya/chart.py
@@ -1,19 +1,21 @@
 import requests
 from os.path import exists
-from os import remove
+from os import error, remove
 import codecs
 import json
 
 
 
 init_chart_path = lambda ID, diff = "expert": diff + "/" + str(ID).zfill(3) + ".json"
+diffs = ["easy", "normal", "hard", "expert", "special"]
 
 
 
 class chart:
-    def __init__(self, ID, diff = "expert") -> None:
-        self.ID = ID
-        self.diff = diff
+    def __init__(self, ID = None, diff = "expert") -> None:
+        self.__ID = ID
+        self.__diff = diff
+        self.__name = ""
         self.path = init_chart_path(ID, diff)
         self.exists = lambda self: exists(self.path)
         self.url = "https://bestdori.com/api/charts/" + str(ID) + "/" + diff + ".json"
@@ -44,3 +46,23 @@ class chart:
     
     def set_path(self, path):
         self.path = path
+    
+    def set_ID(self, ID = None):
+        self.__ID = ID
+    
+    def set_diff(self, diff = "expert"):
+        if diff in diffs:
+            self.__diff = diff
+        else:
+            raise TypeError("Unrecognized difficulty Type \"" + diff + "\"")
+    
+    def set_name(self, name = ""):
+        self.__name = name
+    
+    def get(self): 
+        return self.__ID, self.__diff, self.__name
+    
+    def set(self, ID = None, diff = "expert", name = ""):
+        self.set_ID(ID)
+        self.set_diff(diff)
+        self.set_name(name)
