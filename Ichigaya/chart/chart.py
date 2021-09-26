0a0d2f5012ecd37ad307bbf9339ef385ede83828
LibrarristShalinward <1701427793@qq.com>
2021-09-26 13:03:26 +0800

    完成了各键型的格式化输出

--- Ichigaya/chart/chart.py
+++ Ichigaya/chart/chart.py
@@ -211,10 +211,10 @@ class Chart:
     def get_points(self):
         singles = self.keys["Single"] + self.keys["Flick"] + self.keys["Direct"]
         for point in singles:
-            yield type(point), None, point.beat, point.lane
+            yield type(point), None, point.beat, point.lane, point
         for hold in self.keys["Hold"]:
-            yield key.Hold, True, hold.touch.beat, hold.touch.lane
-            yield key.Hold, False, hold.release.beat, hold.release.lane
+            yield key.Hold, True, hold.touch.beat, hold.touch.lane, hold
+            yield key.Hold, False, hold.release.beat, hold.release.lane, hold
     
     def get_point_list(self):
         return [point for point in self.get_points()]
--- Ichigaya/chart/key.py
+++ Ichigaya/chart/key.py
@@ -1,6 +1,8 @@
 
 
 
+just_len = 10
+
 def json2key(json):
     if not "type" in json.keys():
         return slide(json)
@@ -25,6 +27,9 @@ class Note:
     def json_set(self, json):
         self.beat = float(json["beat"])
         self.lane = float(json["lane"])
+    
+    def __str__(self) -> str:
+        return ("第%.2f拍"%(self.beat)).ljust(just_len) + ("第%i轨"%(self.lane)).ljust(just_len)
 
 class directionalNote(Note):
     def __init__(self, json = None, beat = 0, lane = 0) -> None:
@@ -35,17 +40,35 @@ class directionalNote(Note):
         if hand is not None and self.hand is not None:
             assert hand == self.hand
         else:
-            self.hand == hand
+            self.hand = hand
+    
+    def __str__(self) -> str:
+        if self.hand:
+            if self.hand == "Left":
+                return super().__str__() + "左手".ljust(just_len)
+            else:
+                return super().__str__() + "右手".ljust(just_len)
+        else:
+            return super().__str__()
 
 class Single(directionalNote):
     def __init__(self, json = None, beat = 0, lane = 0) -> None:
         super().__init__(beat=beat, lane=lane, json=json)
         self.lane = int(self.lane)
+    
+    def __str__(self) -> str:
+        return super().__str__()
+    
+    def __repr__(self) -> str:
+        return "单键：".ljust(just_len) + self.__str__()
 
 class Flick(Single):
     def __init__(self, json = None, beat = 0, lane = 0) -> None:
         self.dir = None
         super().__init__(beat = beat, lane = lane, json = json)
+    
+    def __str__(self) -> str:
+        return "滑键：".ljust(just_len) + super().__str__()
 
 class Direct(Flick):
     def __init__(self, json = None, 
@@ -60,6 +83,9 @@ class Direct(Flick):
         super().json_set(json)
         self.dir = json["direction"]
         self.len = json["width"]
+    
+    def __str__(self) -> str:
+        return "滑" + super().__str__()[1:] + ("方向：%s"%("左" if self.dir == "Left" else "右")).ljust(just_len) + ("长度:%i"%(self.len)).ljust(just_len)
 
 
 class slide(directionalNote):
@@ -75,6 +101,9 @@ class slide(directionalNote):
         if "hidden" in json.keys() and json["hidden"]:
             self.visible = False
         else: self.visible = True
+    
+    def __str__(self) -> str:
+        return super().__str__()
 
 class Hold:
     def __init__(self, json = None, notes = [Single(), Single()]) -> None:
@@ -101,6 +130,14 @@ class Hold:
         for s in self.slides:
             s.set_hand(hand)
         self.release.set_hand(hand)
+    
+    def __str__(self) -> str:
+        all_str = "=====\n长键：\n"
+        all_str += self.touch.__str__() + "\n"
+        for s in self.slides:
+            all_str += s.__str__() + "\n"
+        all_str += self.release.__str__()
+        return all_str + "\n====="
 
 class Simo():
     def __init__(self, obj1 = None , obj2 = None, beat = 0, lane = (0, 0)) -> None:
@@ -130,5 +167,8 @@ class Simo():
         return self.is_obj1(note) or self.is_obj2(note)
     
     def set_hand(self):
-        self.obj1.set_hand("Left")
-        self.obj2.set_hand("Right")
+        self.obj1[-1].set_hand("Left")
+        self.obj2[-1].set_hand("Right")
+    
+    def __str__(self) -> str:
+        return "----\n同时对象：\n" + self.obj1.__str__() + "\n" + self.obj2.__str__() + "\n-----"
